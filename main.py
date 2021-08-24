891095a5c94dbbdb0b5efb1641af1800a3fb0ae7
Abdeslam <abdesslamboutaba@gmail.com>
2021-08-24 13:52:46 +0100

    adding multiple items

--- main.py
+++ main.py
@@ -4,43 +4,53 @@ import requests
 from bs4 import BeautifulSoup
 import numpy as np
 from flask import request
+def checkdataexistence(s,li):
+   k=0
+   found=0
+   for e in li:
+    k=k+1
+    if s in e:
+     return str(li[k - 1+9]).split()
+   if (found == 0):
+    return 0
 def getdata(index,dt = []):
  arr=[]
+ names = [
+  'CANADIAN DOLLAR - CHICAGO MERCANTILE EXCHANGE',
+  'SWISS FRANC - CHICAGO MERCANTILE EXCHANGE',
+  'MEXICAN PESO - CHICAGO MERCANTILE EXCHANGE',
+  'BRITISH POUND STERLING - CHICAGO MERCANTILE EXCHANGE',
+  'JAPANESE YEN - CHICAGO MERCANTILE EXCHANGE',
+  'EURO FX - CHICAGO MERCANTILE EXCHANGE',
+  'NEW ZEALAND DOLLAR - CHICAGO MERCANTILE EXCHANGE',
+  'AUSTRALIAN DOLLAR - CHICAGO MERCANTILE EXCHANGE',
+  'U.S. DOLLAR INDEX - ICE FUTURES U.S',
+  'SILVER - COMMODITY EXCHANGE INC',
+  'GOLD - COMMODITY EXCHANGE INC'
+ ]
  if(index=='1'):
-  print("hello")
   for d in dt:
-   print("hello dt")
    URL = "https://www.cftc.gov/sites/default/files/files/dea/cotarchives/20"+d[4:6]+"/futures/deacmesf" + d + ".htm"
    page = requests.get(URL);
    soup = BeautifulSoup(page.content, "html.parser")
    li = soup.prettify().split('\n')
-   i=0
-   for d in li:
-    i=i+1
-    if 'CANADIAN DOLLAR - CHICAGO MERCANTILE EXCHANGE' in d:
-     print(str(li[i-1]))
-     break
-   for a in np.arange(0,6):
-    arr.append(str(li[(i - 1+9)+(21*a)]).split())
-   arr.append(str(li[i - 1 +198]).split())
-   k=0
-   for e in li:
-    k=k+1
-    if 'AUSTRALIAN DOLLAR - CHICAGO MERCANTILE EXCHANGE' in e:
-     arr.append(str(li[k - 1+9]).split())
-     break
+   for o in np.arange(0,8):
+    a=checkdataexistence(names[o],li)
+    if a==0:
+      arr.append(['empty','empty','empty','empty','empty','empty','empty','empty','empty'])
+    else:
+      arr.append(a)
  elif(index=='2'):
   for d in dt:
    URL = "https://www.cftc.gov/sites/default/files/files/dea/cotarchives/20"+d[4:6]+"/futures/deanybtsf"+d+".htm"
    page = requests.get(URL)
    soup = BeautifulSoup(page.content, "html.parser")
    li = soup.prettify().split('\n')
-   k=0
-   for e in li:
-    k=k+1
-    if 'U.S. DOLLAR INDEX - IC E FUTURES U.S' in e:
-     arr.append(str(li[k - 1+9]).split())
-     break
+   a = checkdataexistence(names[8], li)
+   if a == 0:
+    arr.append(['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'])
+   else:
+    arr.append(a)
  else:
    for d in dt:
     URL = "https://www.cftc.gov/sites/default/files/files/dea/cotarchives/20"+d[4:6]+"/futures/deacmxsf" + d + ".htm"
@@ -48,11 +58,12 @@ def getdata(index,dt = []):
 
     soup = BeautifulSoup(page.content, "html.parser")
     li = soup.prettify().split('\n')
-    k = 0
-    for e in li:
-     k = k + 1
-     if ('SILVER - COMMODITY EXCHANGE INC' in e) or ('GOLD - COMMODITY EXCHANGE INC.' in e):
-      arr.append(str(li[k - 1 + 9]).split())
+    for o in np.arange(0, 2):
+     a = checkdataexistence(names[o+9], li)
+     if a == 0:
+      arr.append(['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'])
+     else:
+      arr.append(a)
  return json.dumps(arr)
 #il faut ajouter dix aux nombre de ligne souhaité
 import flask
@@ -64,4 +75,3 @@ app.config["DEBUG"] = True
 @cross_origin(origin='*')
 def home():
     return getdata(request.json['index'],request.json['dt'])
-app.run(port=5000)
